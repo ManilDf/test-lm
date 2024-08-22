276157b261ab9d3b0be95019829bfe5afff20e4a
ManilDf <diaf.manil@gmail.com>
2024-08-22 22:21:39 +0200

    Add createProduit method to ProduitController (second test passed)

--- src/main/java/com/monprojet/product_api/controller/ProduitController.java
+++ src/main/java/com/monprojet/product_api/controller/ProduitController.java
@@ -3,9 +3,7 @@ package com.monprojet.product_api.controller;
 import com.monprojet.product_api.entity.Produit;
 import com.monprojet.product_api.repository.ProduitRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +18,11 @@ public class ProduitController {
     public List<Produit> getAllProduits() {
         return produitRepository.findAllByOrderByNomAsc();
     }
+
+    @PostMapping
+    public Produit createProduit(@RequestBody Produit produit) {
+        return produitRepository.save(produit);
+    }
+
 
 }
