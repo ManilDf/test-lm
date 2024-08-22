4153332eb2e55b51bfa3d70c99630e217cb2a3d0
ManilDf <diaf.manil@gmail.com>
2024-08-22 21:43:23 +0200

    Add getAllProduits method to ProduitController (first test passed)

--- src/main/java/com/monprojet/product_api/controller/ProduitController.java
+++ src/main/java/com/monprojet/product_api/controller/ProduitController.java
@@ -1,4 +1,24 @@
 package com.monprojet.product_api.controller;
 
+import com.monprojet.product_api.entity.Produit;
+import com.monprojet.product_api.repository.ProduitRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/produits")
+@AllArgsConstructor
 public class ProduitController {
+
+    private ProduitRepository produitRepository;
+
+    @GetMapping
+    public List<Produit> getAllProduits() {
+        return produitRepository.findAllByOrderByNomAsc();
+    }
+
 }
